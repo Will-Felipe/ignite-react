0726555eaceb0233567cbf798329cbc686483b9c
Will Felipe <will-felipe@hotmail.com>
2024-09-26 22:23:28 -0400

    estrutura visual

--- dt-money/src/pages/Transactions/index.tsx
+++ dt-money/src/pages/Transactions/index.tsx
@@ -1,9 +1,41 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { SeachForm } from "./components/SearchForm";
+import {
+  PriceHighlight,
+  TransactiosTable,
+  TrasactionsContainer,
+} from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
+      <Summary />
+
+      <TrasactionsContainer>
+        <SeachForm />
+        <TransactiosTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de Site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/09/2024</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHighlight variant="outcome">- R$ 48,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>13/09/2024</td>
+            </tr>
+          </tbody>
+        </TransactiosTable>
+      </TrasactionsContainer>
     </div>
   );
 }
